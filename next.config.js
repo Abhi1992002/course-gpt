d81df66c36b2e847ba2f6c2a6bc3b36d622ef848
abhi1992002 <abhimanyu1992002@gmail.com>
2023-10-03 18:13:16 +0530

    change some nextjs configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
     images:{
         domains:["lh3.googleusercontent.com","s3.us-west-2.amazonaws.com"]
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    typescript:{
+        ignoreBuildErrors: true
+    },
+    output: "standalone"
 }
 
 module.exports = nextConfig
